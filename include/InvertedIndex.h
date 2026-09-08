80985c4288e2ae23e9ae5fc6dc6af1c794aba5b7
Sarthak Bartwal <152021280+batsykods@users.noreply.github.com>
2026-09-08 20:13:40 +0530

    feat: define inverted index

---
+++ include/InvertedIndex.h
@@ -0,0 +1,18 @@
+#ifndef INVERTED_INDEX_H
+#define INVERTED_INDEX_H
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class InvertedIndex {
+private:
+    std::unordered_map<std::string, std::vector<int>> index;
+
+public:
+    void addDocument(int documentId, const std::vector<std::string>& tokens);
+    const std::vector<int>& search(const std::string& term) const;
+    bool contains(const std::string& term) const;
+};
+
+#endif
