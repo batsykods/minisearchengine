2fb5cd0771494e087a91876d521e81e6e87e7f2c
Sarthak Bartwal <152021280+batsykods@users.noreply.github.com>
2026-09-25 18:49:09 +0530

    test: cover repeated pipeline terms

---
+++ tests/coverage_20260925_33.cpp
@@ -0,0 +1,4 @@
+#include <cassert>
+#include "Tokenizer.h"
+#include "InvertedIndex.h"
+int main(){Tokenizer t;InvertedIndex i;i.addDocument(1,t.tokenize("alpha alpha"));assert(i.termFrequency("alpha",1)==2);return 0;}
